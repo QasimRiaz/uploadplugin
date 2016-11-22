574e2a31e7d21736824b60f064495fe96f678747
QasimRiaz <qasim.riaz@e2esp.com>
2016-11-22 16:21:30 +0500

    Add files via upload

---
+++ uploadplugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package uploadplugin
+ * @version 1.6
+ */
+/*
+Plugin Name: Upload Plugin
+Plugin URI: 
+Description: Test Plugin
+Author: Matt Mullenweg
+Version: 1.6
+Author URI: http://ma.tt/
+*/
+
